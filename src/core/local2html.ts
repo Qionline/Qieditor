b28e0fc84e2b76738c58545ba5d02ef66129458a
堂铃 <5516023@qq.com>
2020-11-29 22:14:32 +0800

    💄  预览页滚动条优化

--- src/core/local2html.ts
+++ src/core/local2html.ts
@@ -15,7 +15,13 @@ export const Local2Html = (localSetting: localSettingProp): Local2HtmlProp => {
     localSettingStores.localSettingStore.handleSetScrollPosition(ScrollPositonSetting.num)
   }
   return {
-    ScrollBarHideStyle: `<style>::-webkit-scrollbar {display:none;}</style>`,
+    ScrollBarHideStyle: `<style>
+    ::-webkit-scrollbar {width: 6px;height: 6px;}
+    ::-webkit-scrollbar-thumb {background-color: #b3b3b3;border-radius: 8px;}
+    ::-webkit-scrollbar-thumb:hover {background-color: #999;}
+    ::-webkit-scrollbar-track {background-color: #e3e3e3;border-radius: 8px;}
+    body{overflow-y:auto;overflow-y:overlay;}
+    </style>`,
     TypeDisguise: `<script>${TypeDisguise(localSetting.phoneType)}</script>`,
     ScrollPositonCache: `<script>${ScrollPositonCache(localSetting.scrollPosition)}</script>`,
   }
